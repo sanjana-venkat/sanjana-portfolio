01e01472c9e206073d7531c9b0a3074eb70cd5c1
Sanjana Venkat <94576709+sanjana-venkat@users.noreply.github.com>
2026-06-30 16:19:09 -0700

    Refactor MODEL_DESIGN_INTRO for clarity and conciseness

--- src/App.jsx
+++ src/App.jsx
@@ -1111,10 +1111,9 @@ function TestimonialTile() {
 }
 
 
-const MODEL_DESIGN_INTRO = `I drove work and teams towards launch and learn even in a traditionally slower environment. I was known as the "all-in-one" designer because of my deep collaboration with product, engineering, marketing, senior leadership, and yes, data science and ML teams too.
+const MODEL_DESIGN_INTRO = `I drove work and teams towards launch and learn even in a traditionally slower environment, deeply collaboration with product, engineering, marketing, senior leadership, and yes, data science and ML teams too.
 
-That's why I was so excited to see Model Designer as a role. I'm genuinely more interested in this than visual design, and I want to shape the UX strategy behind model classification, filtering, and response mapping.
-
+That's why I was so excited to see Model Designer as a role, I thrive in thinking about the how and why more than "what should it look like?" 
 At JPMC, I worked across:`;
 
 const MODEL_DESIGN_LINKS = [
